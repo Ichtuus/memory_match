9958d1d1a8062f2b5a49b818ce37d25f52fab8e2
Nicolas <nicolasdiarra@gmail.com>
2023-03-02 23:40:18 +0100

    feat: manage selected card

--- src/components/card/card.vue
+++ src/components/card/card.vue
@@ -1,26 +1,63 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, reactive } from 'vue'
 
-const fruitArray = [
-  { url: '../../../src/assets/fruits/ace.jpg', name: 'Ace' },
-  { url: '../../../src/assets/fruits/baggy.jpg', name: 'Baggy' },
-  { url: '../../../src/assets/fruits/barbeblanche.jpg', name: 'Barbe blanche' },
-  { url: '../../../src/assets/fruits/chopper.jpg', name: 'Chopper' },
-  { url: '../../../src/assets/fruits/doflamingo.jpg', name: 'Doflamingo' },
-  { url: '../../../src/assets/fruits/kaku.jpg', name: 'Kaku' },
-  { url: '../../../src/assets/fruits/kalifa.jpg', name: 'Kalifa' },
-  { url: '../../../src/assets/fruits/law.jpg', name: 'Law' },
-  { url: '../../../src/assets/fruits/luffy.jpg', name: 'Luffy' },
-  { url: '../../../src/assets/fruits/teach.jpg', name: 'Teach' }
+type Card = { url: string; name: string; show: boolean }[]
+
+const maxSelectedCard = 2
+
+let fruitArray: Card = [
+  { url: '../../../src/assets/fruits/ace.jpg', name: 'Ace', show: false },
+  { url: '../../../src/assets/fruits/ace.jpg', name: 'Ace', show: false },
+  { url: '../../../src/assets/fruits/baggy.jpg', name: 'Baggy', show: false },
+  { url: '../../../src/assets/fruits/baggy.jpg', name: 'Baggy', show: false },
+  { url: '../../../src/assets/fruits/barbeblanche.jpg', name: 'Barbe blanche', show: false },
+  { url: '../../../src/assets/fruits/barbeblanche.jpg', name: 'Barbe blanche', show: false },
+  { url: '../../../src/assets/fruits/chopper.jpg', name: 'Chopper', show: false },
+  { url: '../../../src/assets/fruits/chopper.jpg', name: 'Chopper', show: false },
+  { url: '../../../src/assets/fruits/doflamingo.jpg', name: 'Doflamingo', show: false },
+  { url: '../../../src/assets/fruits/doflamingo.jpg', name: 'Doflamingo', show: false },
+  { url: '../../../src/assets/fruits/kaku.jpg', name: 'Kaku', show: false },
+  { url: '../../../src/assets/fruits/kaku.jpg', name: 'Kaku', show: false },
+  { url: '../../../src/assets/fruits/kalifa.jpg', name: 'Kalifa', show: false },
+  { url: '../../../src/assets/fruits/kalifa.jpg', name: 'Kalifa', show: false },
+  { url: '../../../src/assets/fruits/law.jpg', name: 'Law', show: false },
+  { url: '../../../src/assets/fruits/law.jpg', name: 'Law', show: false },
+  { url: '../../../src/assets/fruits/luffy.jpg', name: 'Luffy', show: false },
+  { url: '../../../src/assets/fruits/luffy.jpg', name: 'Luffy', show: false },
+  { url: '../../../src/assets/fruits/teach.jpg', name: 'Teach', show: false },
+  { url: '../../../src/assets/fruits/teach.jpg', name: 'Teach', show: false }
 ]
 
-let showSide = false
+let selectedCard = ref(0)
+let countSelectedCard = 0
 
-onMounted(() => {})
-
-function testshow(e: any) {
-  console.log('event', e)
+function selectFruit(event: any) {
+  if (event.target) {
+    const { cardindex } = event.target?.dataset
+    console.log('cardindex', cardindex)
+    selectedCard.value = cardindex
+    countSelectedCard += 1
+    updateFruitArray(cardindex)
+  }
 }
+
+function updateFruitArray(indexcard: number) {
+  if (countSelectedCard != maxSelectedCard) {
+    fruitArray.forEach((fruit) => (fruit.show = false))
+  } else {
+    countSelectedCard = 0
+  }
+
+  const element = fruitArray.find((fruit, index) => index == indexcard)
+
+  if (element) {
+    element.show = true
+  }
+}
+
+onMounted(() => {
+  fruitArray.sort(() => Math.random() - 0.5)
+})
 </script>
 
 <template>
@@ -31,11 +68,16 @@ function testshow(e: any) {
         :key="index"
         class="ds-col__12-s ds-col__3-l card__body"
       >
-        <div class="card__body-img" @click="testshow()">
+        <div
+          class="card__body-img"
+          :class="{ selected: selectedCard === index }"
+          @click="selectFruit"
+        >
           <figure>
             <img
+              :data-cardindex="index"
               :alt="fruit.name"
-              :src="showSide ? fruit.url : '../../../src/assets/pngegg.png'"
+              :src="fruit.show ? fruit.url : '../../../src/assets/pngegg.png'"
               height="322"
               width="270"
             />
